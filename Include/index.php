6145294f445098943a56dd72c6084fbf326975fe
1kaleab <1kaleabtesfaye@gmail.com>
2022-11-16 08:48:35 -0500

    database connection and include file created

---
+++ Include/index.php
@@ -0,0 +1,28 @@
+<?php
+
+//include database connection
+
+// registration
+
+// login
+
+// create candidate
+
+// view candidate
+
+// update candidate
+
+// delete candidate
+
+// vote 
+
+// create voter
+
+// view voter
+
+// update voter
+
+// delete voter
+
+
+?> 
