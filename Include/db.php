eaf13d1019da0b81029aec648e7b34d478884ef8
Abenezer Tesfaye <abenezertesfaye98@gmail.com>
2022-11-17 16:29:39 +0300

    edited db.php

--- Include/db.php
+++ Include/db.php
@@ -1,3 +1,5 @@
+<?php
+
 $servername = "localhost";
 $username = "root";
 $password = "password";
@@ -10,4 +12,6 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully"
+echo "Connected successfully";
+
+?>
